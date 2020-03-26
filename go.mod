97b3d3ec4a460ad332213c34093628992292a6a2
Jan-Christoph Kuester <jckuester@gmail.com>
2020-03-26 12:02:07 +0100

    Get name of the file that implements a resource
    Returns "resource_aws_vpc.go" for a given type "aws_vpc",
    for example.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jckuester/terratools
 
 go 1.13
 
-require github.com/apex/log v1.1.2
+require (
+	github.com/apex/log v1.1.2
+	github.com/aws/aws-sdk-go v1.20.6
+)
--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"io/ioutil"
 	"strings"
 
-	"github.com/apex/log"
+	"github.com/aws/aws-sdk-go/aws"
+
 	"github.com/apex/log/handlers/cli"
+
+	"github.com/apex/log"
 )
 
 func main() {
@@ -16,8 +20,14 @@ func main() {
 
 	resourceTypes := ResourceTypes()
 	log.Infof("Number of Terraform AWS resource types: %d", len(resourceTypes))
+
 	for _, r := range resourceTypes {
-		log.Debugf(r)
+		fileName := GetResourceTypeFileName(r)
+		if fileName != nil {
+			log.Debugf("%s: %s", r, *fileName)
+		} else {
+			log.Debugf(r)
+		}
 	}
 }
 
@@ -67,5 +77,75 @@ func ResourceTypes() []string {
 		return true
 	})
 
+	return result
+}
+
+// GetResourceTypeFileName returns the name of the file that implements the resource of the given resource type.
+func GetResourceTypeFileName(resourceType string) *string {
+	node, err := parser.ParseFile(token.NewFileSet(),
+		"/home/jan/git/github.com/yoyolabsio/terraform-provider-aws/aws/provider.go",
+		nil, 0)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	var result *string
+
+	ast.Inspect(node, func(n ast.Node) bool {
+		mapEntry, ok := n.(*ast.KeyValueExpr)
+		if !ok {
+			return true
+		}
+
+		key, ok := mapEntry.Key.(*ast.BasicLit)
+		if !ok {
+			return true
+		}
+
+		if resourceType != strings.Trim(key.Value, "\"") {
+			return false
+		}
+
+		resource, ok := mapEntry.Value.(*ast.CallExpr)
+		if !ok {
+			return false
+		}
+
+		resourceFuncName, ok := resource.Fun.(*ast.Ident)
+		if !ok {
+			return false
+		}
+
+		files, err := ioutil.ReadDir("/home/jan/git/github.com/yoyolabsio/terraform-provider-aws/aws")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		for _, f := range files {
+			if strings.HasPrefix(f.Name(), "resource_aws") {
+				node, err := parser.ParseFile(token.NewFileSet(),
+					"/home/jan/git/github.com/yoyolabsio/terraform-provider-aws/aws/"+f.Name(),
+					nil, 0)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+
+				ast.Inspect(node, func(n ast.Node) bool {
+					fn, ok := n.(*ast.FuncDecl)
+					if !ok {
+						return true
+					}
+
+					if fn.Name.Name == resourceFuncName.Name {
+						result = aws.String(f.Name())
+						return false
+					}
+
+					return true
+				})
+			}
+		}
+		return true
+	})
 	return result
 }
